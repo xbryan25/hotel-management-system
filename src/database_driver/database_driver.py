5ca8a689168e03e9c60aba992f35aa56b9644fe3
Bryan <bryanagan25@gmail.com>
2025-04-08 11:48:19 +0800

    Add database_driver and initialize_database, tables will be created if it doesn't exist yet

---
+++ src/database_driver/database_driver.py
@@ -0,0 +1,33 @@
+import pymysql
+
+from database_driver.initialize_database import InitializeDatabase
+
+
+class DatabaseDriver:
+
+    def __init__(self):
+
+        db_initial_user_values = ["root", "root"]
+
+        input_user = input("Input user (default: localhost): ")
+        input_password = input("Input password (default: localhost): ")
+
+        if input_user.strip() != "":
+            db_initial_user_values[0] = input_user
+
+        if input_password.strip() != "":
+            db_initial_user_values[1] = input_password
+
+        self.db = pymysql.connect(
+            host='localhost',
+            user=db_initial_user_values[0],
+            password=db_initial_user_values[1],
+            database='hms_db'
+        )
+
+        self.cursor = self.db.cursor()
+
+        InitializeDatabase(self.cursor)
+
+
+
