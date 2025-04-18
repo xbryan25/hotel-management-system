bbc1ca767232c085869034404ec7ef880b2f3c7f
Bryan <bryanagan25@gmail.com>
2025-04-18 13:59:14 +0800

    feat(database_driver): add create, update, and delete operations for services

--- src/database_driver/database_driver.py
+++ src/database_driver/database_driver.py
@@ -148,6 +148,55 @@ class DatabaseDriver:
         self.cursor.execute(sql, values)
         self.db.commit()
 
+    def get_latest_service_id(self):
+
+        self.cursor.execute("""SELECT service_id FROM services
+            ORDER BY CAST(SUBSTRING(service_id, 9) AS UNSIGNED) DESC LIMIT 1;
+        """)
+
+        result = self.cursor.fetchone()
+
+        if not result:
+            return "service-000"
+        else:
+            return result[0]
+
+    def add_service(self, service_information):
+        sql = """INSERT INTO services
+                (service_id, service_name, rate) VALUES
+                (%s, %s, %s)"""
+
+        latest_service_id = self.get_latest_service_id()
+
+        new_service_id = f"service-{int(latest_service_id[9:]) + 1:03}"
+
+        values = (new_service_id,
+                  service_information[0],
+                  service_information[1])
+
+        self.cursor.execute(sql, values)
+        self.db.commit()
+
+    def update_service(self, old_service_id, service_information):
+        sql = """UPDATE services SET service_id=%s, service_name=%s, rate=%s
+        WHERE service_id=%s;"""
+
+        values = (service_information[0],
+                  service_information[1],
+                  service_information[2],
+                  old_service_id)
+
+        self.cursor.execute(sql, values)
+        self.db.commit()
+
+    def delete_service(self, identifier):
+
+        sql = "DELETE FROM services WHERE service_id=%s"
+        values = (identifier.strip(),)
+
+        self.cursor.execute(sql, values)
+        self.db.commit()
+
     def check_environment_variables(self):
         if not all([os.getenv("DB_HOST"), os.getenv("DB_USER"), os.getenv("DB_PASSWORD")]):
             raise EnvironmentError("Missing one or more required DB environment variables.")
