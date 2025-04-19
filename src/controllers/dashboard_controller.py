6475693179500f668eaab8c82e343ffefcbb04e4
Bryan <bryanagan25@gmail.com>
2025-04-19 09:39:53 +0800

    refactor(dashboard): change table widgets in dashboard page to table views

--- src/controllers/dashboard_controller.py
+++ src/controllers/dashboard_controller.py
@@ -1,6 +1,15 @@
+from models.recent_stays_model import RecentStaysModel
+from models.reservation_model import ReservationModel
 
 
 class DashboardController:
     def __init__(self, page_widget, db_driver):
         self.view = page_widget
         self.db_driver = db_driver
+
+        self.set_models()
+
+    def set_models(self):
+        self.view.recent_check_in_frame_table_view.setModel(RecentStaysModel())
+        self.view.recent_check_out_frame_table_view.setModel(RecentStaysModel())
+        self.view.reservation_list_frame_table_view.setModel(ReservationModel())
