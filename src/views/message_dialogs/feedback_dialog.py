35da24bb153b151fb8b01284369d3d2466b03d4d
Bryan <bryanagan25@gmail.com>
2025-05-09 15:57:35 +0800

    feat(feedback_dialog): add subheader_message_label

--- src/views/message_dialogs/feedback_dialog.py
+++ src/views/message_dialogs/feedback_dialog.py
@@ -5,21 +5,26 @@ from ui import FeedbackDialogUI
 
 
 class FeedbackDialog(QDialog, FeedbackDialogUI):
-    def __init__(self, header_message, connected_view=None, proceed_button_text=None):
+    def __init__(self, header_message, subheader_message=None, connected_view=None, proceed_button_text=None):
         super().__init__()
 
         self.setupUi(self)
 
         self.connected_view = connected_view
 
-        self.set_messages(header_message, proceed_button_text)
+        self.set_messages(header_message, subheader_message, proceed_button_text)
         self.connect_signals_to_slots()
 
         self.load_fonts()
         self.set_external_stylesheet()
 
-    def set_messages(self, header_message, proceed_button_text):
+    def set_messages(self, header_message, subheader_message, proceed_button_text):
         self.header_message_label.setText(header_message)
+
+        if not subheader_message:
+            self.subheader_message_label.setVisible(False)
+        else:
+            self.subheader_message_label.setText(subheader_message)
 
         if not proceed_button_text:
             self.proceed_button.setText("Proceed")
@@ -41,5 +46,6 @@ class FeedbackDialog(QDialog, FeedbackDialogUI):
 
     def load_fonts(self):
         self.header_message_label.setFont(QFont("Inter", 14, QFont.Weight.Bold))
+        self.subheader_message_label.setFont(QFont("Inter", 12, QFont.Weight.Normal))
 
         self.proceed_button.setFont(QFont("Inter", 15, QFont.Weight.Bold))
