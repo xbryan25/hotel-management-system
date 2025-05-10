3de48ff6448c6ced73767746c92e113972ec0809
Bryan <bryanagan25@gmail.com>
2025-05-10 11:43:32 +0800

    feat(booking_page): add UI elements designed in Qt Designer, with style and Inter font

--- src/views/pages/booking_page.py
+++ src/views/pages/booking_page.py
@@ -1,4 +1,5 @@
 from PyQt6.QtWidgets import QWidget
+from PyQt6.QtGui import QFont
 
 from ui import BookingPageUI
 
@@ -8,3 +9,26 @@ class BookingPage(QWidget, BookingPageUI):
         super().__init__()
 
         self.setupUi(self)
+
+        self.set_external_stylesheet()
+        self.load_fonts()
+
+    def set_external_stylesheet(self):
+        with open("../resources/styles/booking_page.qss", "r") as file:
+            self.setStyleSheet(file.read())
+
+    def load_fonts(self):
+        self.search_lineedit.setFont(QFont("Inter", 16, QFont.Weight.Normal))
+
+        self.bookings_label.setFont(QFont("Inter", 20, QFont.Weight.Bold))
+        self.date_today_label.setFont(QFont("Inter", 18, QFont.Weight.Normal))
+
+        self.view_type_combobox.setFont(QFont("Inter", 12, QFont.Weight.Normal))
+        self.sort_by_combobox.setFont(QFont("Inter", 12, QFont.Weight.Normal))
+        self.sort_type_combobox.setFont(QFont("Inter", 12, QFont.Weight.Normal))
+
+        self.bookings_table_view.setFont(QFont("Inter", 10, QFont.Weight.Normal))
+        self.bookings_table_view.horizontalHeader().setFont(QFont("Inter", 14, QFont.Weight.Bold))
+
+        self.previous_page_button.setFont(QFont("Inter", 11, QFont.Weight.Normal))
+        self.next_page_button.setFont(QFont("Inter", 11, QFont.Weight.Normal))
