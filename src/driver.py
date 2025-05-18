be81f8dbfb6a4d0b54d682044db7286428e0020c
esykeal <vinceeezekiel.jandayan@g.msuiit.edu.ph>
2025-05-18 22:28:10 +0800

    edited the qss

--- src/driver.py
+++ src/driver.py
@@ -1,17 +1,23 @@
 from PyQt6.QtWidgets import QApplication
+from PyQt6.QtGui import QPalette, QColor
+import sys
 
 from views.application_window import ApplicationWindow
 
-
 def main():
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
 
-    app = QApplication([])
+    palette = QPalette()
+    palette.setColor(QPalette.ColorRole.Window, QColor("#F1F1F1"))
+    palette.setColor(QPalette.ColorRole.Base, QColor("#F1F1F1"))
+    palette.setColor(QPalette.ColorRole.WindowText, QColor("#000000"))
+    app.setPalette(palette)
 
     application_window = ApplicationWindow()
-
     application_window.show()
-    app.exec()
 
+    app.exec()
 
 if __name__ == "__main__":
     main()
