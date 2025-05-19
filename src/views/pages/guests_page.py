b54be4e16f9eec487c28a20eb78613b12e4deb04
esykeal <vinceeezekiel.jandayan@g.msuiit.edu.ph>
2025-05-19 21:15:41 +0800

    edited the guest_info_dialogs and with several dialogs qss

--- src/views/pages/guests_page.py
+++ src/views/pages/guests_page.py
@@ -43,6 +43,7 @@ class GuestsPage(QWidget, GuestsPageUI):
         guest_table_view_header.setStyleSheet("""
             QHeaderView::section {
                 background-color: #FFFFFF;
+                color: #000000;
                 border: none;
                 outline: none;
             }
